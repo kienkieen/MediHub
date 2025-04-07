77fe45b00bcf0875232e7721065a9d103a6720ef
kienkieen <trungkiena1206052004@gmail.com>
2025-04-07 20:04:46 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MediHub Login',
+      title: 'MediHub App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF0091FF),
